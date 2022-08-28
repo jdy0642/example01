eb5fe4efca61cb40e95c92aee9df28ec061b8d44
jdy <jdy0642@gmail.com>
2022-08-29 02:52:35 +0900

    practice 01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Subject from './components/Subject'
+import TOC from './components/TOC'
+import Content from './components/Content'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: {title: 'WEB', sub: 'World Wide Web2'},
+      contents: [
+        {id:1, title:'HTML', desc:'HTML is ~'},
+        {id:2, title:'CSS', desc:'CSS is ~'},
+        {id:3, title:'JS', desc:'JS is ~'}
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title="HTML" desc="HTML is Hyper~~"></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
